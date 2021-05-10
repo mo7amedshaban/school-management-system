ebee3375e3f9dffb3f020afffc7cb6c68cdd049e
Mohamed Shaban <mohamed.2015.ms880@gmail.com>
2021-05-10 05:27:40 +0200

    Grades

---
+++ Notes.md
@@ -0,0 +1,13 @@
+#### install Authenticaion in laravel
+
+#### mcamara
+
+* change language of website to ar or en
+
+#### spatie/laravel-translatable
+
+* for insert english and arabic in database
+
+#### yoeunes / toastr
+
+* for notification when isert success or fail
---
+++ app/Http/Controllers/Grades/GradeController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Grades;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreGrades;
+use App\Models\Grade;
+use Illuminate\Http\Request;
+
+class GradeController extends Controller
+{
+
+    public function index()
+    {
+        $Grades = Grade::all();
+        return view('pages.Grades.Grades', compact('Grades'));
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(StoreGrades $request)
+    {
+        # install package  "spatie/laravel-translatable"
+        try {
+            $Grade = new Grade();
+            /*
+             $translations = [
+                 'en' => $request->Name_en,
+                 'ar' => $request->Name
+             ];
+             $Grade->setTranslations('Name', $translations);
+             */
+            $Grade->Name = ['en' => $request->Name_en, 'ar' => $request->Name];
+            $Grade->Notes = $request->Notes;
+            $Grade->save();
+            # install package "yoeunes/toastr"
+            toastr()->success(trans('messages.success'));
+            return redirect()->route('Grades.index');
+        } catch (\Exception $e) {
+            return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+        }
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+
+    public function edit($id)
+    {
+        //
+    }
+
+
+    public function update(Request $request)
+    {
+        try {
+
+//            $validated = $request->validated();
+            $Grades = Grade::findOrFail($request->id);
+            $Grades->update([
+                $Grades->Name = ['ar' => $request->Name, 'en' => $request->Name_en],
+                $Grades->Notes = $request->Notes,
+            ]);
+            toastr()->success(trans('messages.Update'));
+            return redirect()->route('Grades.index');
+        } catch
+        (\Exception $e) {
+            return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+        }
+    }
+
+
+    public function destroy(Request $request)
+    {
+//        $MyClass_id = Classroom::where('Grade_id',$request->id)->pluck('Grade_id');
+
+//        if($MyClass_id->count() == 0){
+
+        $Grades = Grade::findOrFail($request->id)->delete();
+        toastr()->error(trans('messages.Delete'));
+        return redirect()->route('Grades.index');
+//        }
+
+//        else{
+//
+//            toastr()->error(trans('Grades_trans.delete_Grade_Error'));
+//            return redirect()->route('Grades.index');
+//
+//        }
+
+    }
+}
