a563af104676047d0e5d458a83cc39b603947840
Mohamed Shaban <mohamed.2015.ms880@gmail.com>
2021-10-02 14:39:45 +0200

    edit in tablessss

--- database/migrations/2021_08_13_022610_create_promotions_table.php
+++ database/migrations/2021_08_13_022610_create_promotions_table.php
@@ -29,11 +29,11 @@ class CreatePromotionsTable extends Migration
 
         Schema::table('promotions', function (Blueprint $table) {
             $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
-            $table->foreign('from_grade')->references('id')->on('Grades')->onDelete('cascade');
-            $table->foreign('from_Classroom')->references('id')->on('Classrooms')->onDelete('cascade');
+            $table->foreign('from_grade')->references('id')->on('grades')->onDelete('cascade');
+            $table->foreign('from_Classroom')->references('id')->on('classrooms')->onDelete('cascade');
             $table->foreign('from_section')->references('id')->on('sections')->onDelete('cascade');
-            $table->foreign('to_grade')->references('id')->on('Grades')->onDelete('cascade');
-            $table->foreign('to_Classroom')->references('id')->on('Classrooms')->onDelete('cascade');
+            $table->foreign('to_grade')->references('id')->on('grades')->onDelete('cascade');
+            $table->foreign('to_Classroom')->references('id')->on('classrooms')->onDelete('cascade');
             $table->foreign('to_section')->references('id')->on('sections')->onDelete('cascade');
         });
     }
