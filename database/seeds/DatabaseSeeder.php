7550d8aa20b2aeb07f41df815d136e1075fbb7b2
Mohamed Shaban <mohamed.2015.ms880@gmail.com>
2021-10-02 17:29:25 +0200

    break 222

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         $this->call(GenderTableSeeder::class);
 
 //        $this->call(ParentsTableSeeder::class);
-        $this->call(StudentsTableSeeder::class);
+//        $this->call(StudentsTableSeeder::class);
 
 
     }
