8cd44d9abacf4dac6134ccd463a2458ba4134da1
Mohamed Shaban <mohamed.2015.ms880@gmail.com>
2021-10-02 13:33:39 +0200

    add psgl database

--- config/database.php
+++ config/database.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('DATABASE_URL');
 
 return [
 
@@ -16,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
