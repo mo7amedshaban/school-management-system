10f4a0a65511e1b6bae19d781ffd481f1eed6418
Mohamed Shaban <mohamed.2015.ms880@gmail.com>
2022-05-22 19:54:07 +0300

    edit in database file

--- config/database.php
+++ config/database.php
@@ -1,10 +1,6 @@
 <?php
 
-use Illuminate\Support\Str;
-
-//$DATABASE_URL = parse_url('DATABASE_URL');
-$DATABASE_URL = parse_url('postgres://dvpexgwkvctwfl:998d435e06151416f6e6f0c9135c774681f32fd3f9e6652c37c9d1e55977fc83@ec2-107-22-238-112.compute-1.amazonaws.com:5432/dan26g38vi5h62');
-
+$DATABASE_URL = parse_url('postgres://rjjrcczfcfugkn:3da7c2aeb8b96ee2ece534af65ec61deb794f80dedec04793de5e663073952ca@ec2-3-231-82-226.compute-1.amazonaws.com:5432/d1c4ka2jq43jj4');
 
 return [
 
@@ -41,15 +37,12 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -59,26 +52,10 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
         ],
 
-//        'pgsql' => [
-//            'driver' => 'pgsql',
-//            'host' => isset($DATABASE_URL["host"]) ? $DATABASE_URL["host"] : null,
-//            'port' => isset($DATABASE_URL["port"]) ? $DATABASE_URL["port"] : null,
-//            'database' => isset($DATABASE_URL["path"]) ? ltrim($DATABASE_URL["path"], "/") : null,
-//            'username' => isset($DATABASE_URL["user"]) ? $DATABASE_URL["user"] : null,
-//            'password' => isset($DATABASE_URL["pass"]) ? $DATABASE_URL["pass"] : null,
-//            'charset' => 'utf8',
-//            'prefix' => '',
-//            'schema' => 'public',
-//            'sslmode' => 'require',
-//        ],
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => $DATABASE_URL["host"],
@@ -94,15 +71,13 @@ return [
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '1433'),
             'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
         ],
 
     ],
@@ -126,166 +101,22 @@ return [
     |--------------------------------------------------------------------------
     |
     | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
+    | provides a richer set of commands than a typical key-value systems
     | such as APC or Memcached. Laravel makes it easy to dig right in.
     |
     */
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
-
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
-        ],
+        'client' => 'predis',
 
         'default' => [
-            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
-        ],
-
-        'cache' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 0,
         ],
 
     ],
 
 ];
-
-
-
-
-
-
-
-
-//use Illuminate\Support\Str;
-//$DATABASE_URL=parse_url('postgres://vasjhekqsvgjrs:7acc2a1ba4d88727802f4f4d2e0c98e03f3ac6d976caf0f7673bd3c2f885adaf@ec2-52-86-25-51.compute-1.amazonaws.com:5432/d20o647c09r1hf');
-//
-//
-//return [
-//
-//    /*
-//    |--------------------------------------------------------------------------
-//    | Default Database Connection Name
-//    |--------------------------------------------------------------------------
-//    |
-//    | Here you may specify which of the database connections below you wish
-//    | to use as your default connection for all database work. Of course
-//    | you may use many connections at once using the Database library.
-//    |
-//    */
-//
-//    'default' => env('DB_CONNECTION', 'pgsql'),
-//
-//    /*
-//    |--------------------------------------------------------------------------
-//    | Database Connections
-//    |--------------------------------------------------------------------------
-//    |
-//    | Here are each of the database connections setup for your application.
-//    | Of course, examples of configuring each database platform that is
-//    | supported by Laravel is shown below to make development simple.
-//    |
-//    |
-//    | All database work in Laravel is done through the PHP PDO facilities
-//    | so make sure you have the driver for your particular database of
-//    | choice installed on your machine before you begin development.
-//    |
-//    */
-//
-//    'connections' => [
-//
-//        'sqlite' => [
-//            'driver' => 'sqlite',
-//            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-//            'prefix' => '',
-//        ],
-//
-//        'mysql' => [
-//            'driver' => 'mysql',
-//            'host' => env('DB_HOST', '127.0.0.1'),
-//            'port' => env('DB_PORT', '3306'),
-//            'database' => env('DB_DATABASE', 'forge'),
-//            'username' => env('DB_USERNAME', 'forge'),
-//            'password' => env('DB_PASSWORD', ''),
-//            'unix_socket' => env('DB_SOCKET', ''),
-//            'charset' => 'utf8mb4',
-//            'collation' => 'utf8mb4_unicode_ci',
-//            'prefix' => '',
-//            'strict' => true,
-//            'engine' => null,
-//        ],
-//
-//        'pgsql' => [
-//            'driver' => 'pgsql',
-//            'host' => $DATABASE_URL["host"],
-//            'port' => $DATABASE_URL["port"],
-//            'database' => ltrim($DATABASE_URL["path"], "/"),
-//            'username' => $DATABASE_URL["user"],
-//            'password' => $DATABASE_URL["pass"],
-//            'charset' => 'utf8',
-//            'prefix' => '',
-//            'schema' => 'public',
-//            'sslmode' => 'require',
-//        ],
-//
-//        'sqlsrv' => [
-//            'driver' => 'sqlsrv',
-//            'host' => env('DB_HOST', 'localhost'),
-//            'port' => env('DB_PORT', '1433'),
-//            'database' => env('DB_DATABASE', 'forge'),
-//            'username' => env('DB_USERNAME', ''),
-//            'password' => env('DB_PASSWORD', ''),
-//            'charset' => 'utf8',
-//            'prefix' => '',
-//        ],
-//
-//    ],
-//
-//    /*
-//    |--------------------------------------------------------------------------
-//    | Migration Repository Table
-//    |--------------------------------------------------------------------------
-//    |
-//    | This table keeps track of all the migrations that have already run for
-//    | your application. Using this information, we can determine which of
-//    | the migrations on disk haven't actually been run in the database.
-//    |
-//    */
-//
-//    'migrations' => 'migrations',
-//
-//    /*
-//    |--------------------------------------------------------------------------
-//    | Redis Databases
-//    |--------------------------------------------------------------------------
-//    |
-//    | Redis is an open source, fast, and advanced key-value store that also
-//    | provides a richer set of commands than a typical key-value systems
-//    | such as APC or Memcached. Laravel makes it easy to dig right in.
-//    |
-//    */
-//
-//    'redis' => [
-//
-//        'client' => 'predis',
-//
-//        'default' => [
-//            'host' => env('REDIS_HOST', '127.0.0.1'),
-//            'password' => env('REDIS_PASSWORD', null),
-//            'port' => env('REDIS_PORT', 6379),
-//            'database' => 0,
-//        ],
-//
-//    ],
-//
-//];
